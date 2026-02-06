3e7ed671f116e5bc826337596be543a50993d414
ttoram <rorensiaverisca@gmail.com>
2026-02-06 11:11:38 +0700

    week01: add calculateStatus function

--- src/oop_00000110629_RorensiaVerisca/GradingSystem.kt
+++ src/oop_00000110629_RorensiaVerisca/GradingSystem.kt
@@ -13,5 +13,7 @@ fun main() {
         else -> "D"
     }
     println("Grade Kamu: $grade")
+    println("Status: ${calculateStatus(score)}")
 }
+fun calculateStatus(score: Int)= if (score > 75) "Lulus" else "Tidak Lulus"
 
