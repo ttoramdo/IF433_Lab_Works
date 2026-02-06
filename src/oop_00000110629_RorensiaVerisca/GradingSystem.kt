c4f07492c8773cd076f242e1f9810b8e954e16ad
ttoram <rorensiaverisca@gmail.com>
2026-02-06 11:08:44 +0700

    week01: implement grading logic using when expression

--- src/oop_00000110629_RorensiaVerisca/GradingSystem.kt
+++ src/oop_00000110629_RorensiaVerisca/GradingSystem.kt
@@ -6,3 +6,11 @@ fun main() {
 
     println("Nama: $name , Nilai: $score")
 }
+
+val grade = when (score) {
+    in 90..100 -> "A"
+    in 80..89 -> "B"
+    in 70..79 -> "C"
+    else -> "D"
+}
+println("Grade Kamu: $grade")
