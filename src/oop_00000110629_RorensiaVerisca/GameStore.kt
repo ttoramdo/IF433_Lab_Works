8e5b8d18f4c769eb0bb714bac8982718b1561883
ttoram <rorensiaverisca@gmail.com>
2026-02-12 22:50:46 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000110629_RorensiaVerisca/GameStore.kt
+++ src/oop_00000110629_RorensiaVerisca/GameStore.kt
@@ -2,7 +2,10 @@ package oop_00000110629_RorensiaVerisca
 
 fun main() {
     val gameTitle = "Puzzle Seventeen"
-    val price = 17000
+    val price = 1700000
     println("Game:  $gameTitle\nPrice: $price")
 }
+fun calculateDiscount (price:Int) =
+    if (price > 500000)
+            (price * 80) /100 else  (price * 90) / 100
 
