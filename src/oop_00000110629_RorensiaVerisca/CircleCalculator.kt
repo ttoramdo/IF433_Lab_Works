59157515eaecb1e2d112b0b35ff47a41b988059b
ttoram <rorensiaverisca@gmail.com>
2026-02-12 22:10:54 +0700

    week01: (tugas 1) add legalecy CircleCalculator code

--- src/oop_00000110629_RorensiaVerisca/CircleCalculator.kt
+++ src/oop_00000110629_RorensiaVerisca/CircleCalculator.kt
@@ -1,18 +1,20 @@
 package oop_00000110629_RorensiaVerisca
-
-fun main() {
-    val radius = 2.0
-    val pi = 3.14
-    val area = pi * radius * radius
-
-    println("Radius : $radius, Area : $area")
+fun main(args: Array<String>) {
+    // Variable definition
+    var radius: Double = 7.0
+    var pi: Double = 3.14
+    // Calculation
+    var area: Double = pi * radius * radius
+// Output
+    println("Radius: " + radius + ", Area: " + area)
+    // Logic check
     println(checkSize(area))
 }
 fun checkSize(area: Double) {
-    if (area > 100){
-        println("This a Big Circle")
-    }else {
-        println("This a Small Circle")
+    if (area > 100) {
+        println("This is a Big Circle")
+    } else {
+        println("This is a Small Circle")
     }
 }
 
