b1c3774aad42d957b97e8e26e4771b18218aa583
ttoram <rorensiaverisca@gmail.com>
2026-02-12 23:07:40 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000110629_RorensiaVerisca/GameStore.kt
+++ src/oop_00000110629_RorensiaVerisca/GameStore.kt
@@ -3,9 +3,14 @@ package oop_00000110629_RorensiaVerisca
 fun main() {
     val gameTitle = "Puzzle Seventeen"
     val price = 1700000
-    println("Game:  $gameTitle\nPrice: $price")
+    val finalPrice = calculateDiscount(price)
+    printReceipt(gameTitle=gameTitle, finalPrice=finalPrice, price=price)
+
 }
-fun calculateDiscount (price:Int) =
+fun calculateDiscount (price:Int ) =
     if (price > 500000)
             (price * 80) /100 else  (price * 90) / 100
 
+fun printReceipt (gameTitle: String, finalPrice: Int, price: Int) {
+    println("=======RECEIPT=======\nGame: $gameTitle\nNormal Price: $price\nPrice After Disc: $finalPrice\n=====================" )
+}
