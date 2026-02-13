fbfd4226f80d97fef4092ddfcd12e12b508c9c59
ttoram <rorensiaverisca@gmail.com>
2026-02-13 19:13:23 +0700

    week02: add secondary constructor

--- src/week2/Student.kt
+++ src/week2/Student.kt
@@ -13,5 +13,7 @@ class Student (
             println("test")
         }
     }
-
+    constructor(name: String, nim: String) : this (name, nim, "Non-Matriculated"){
+        println("LOG : Menggunkan constructor jalur umum (Tanpa Jurusan).")
+    }
 }
