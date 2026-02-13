0356e4e64064d82c6902665f638aa2057324ae19
ttoram <rorensiaverisca@gmail.com>
2026-02-13 19:42:52 +0700

    week02: implement selection logic for constructors

--- src/week2/Main.kt
+++ src/week2/Main.kt
@@ -11,12 +11,33 @@ fun main() {
 
     print("Masukkan NIM (Wajib 5 Karakter): ")
     val nim = scanner.next()
-
     scanner.nextLine()
 
     if(nim.length != 5){
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
+    } else {
+        print("Masukkan Jurusan: ")
         val major = scanner.nextLine()
         val s1 = Student(name, nim, major)
         println("status : Pendaftaran Selesai")
     }
+    print("Pilih Jalur (1. Reguler, 2. Umum): ")
+    val type = scanner.nextInt()
+    scanner.nextLine()
+
+    if (type == 1) {
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine()
+        val s1 = Student(name, nim, major)
+        println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+    } else if (type == 2) {
+        val s2 = Student(name, nim)
+        println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+    } else{
+        println("Pilihan ngawur, pendaftaran batal!")
+    }
+
+
 }
+
+
