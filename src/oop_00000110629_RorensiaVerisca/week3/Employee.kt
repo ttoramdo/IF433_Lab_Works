2246b761eea4782a92b59ab9e56a6132ff815cfd
ttoram <rorensiaverisca@gmail.com>
2026-02-20 10:58:17 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000110629_RorensiaVerisca/week3/Employee.kt
+++ src/oop_00000110629_RorensiaVerisca/week3/Employee.kt
@@ -1,5 +1,11 @@
 package oop_00000110629_RorensiaVerisca.week3
 
-class Employee (val name: String){
+class Employee(val name: String){
     var salary: Int = 0
 }
+
+var salary: Int = 0
+    set(value) {
+        println("Mencoba set gaji ke: $value")
+        this.salary = value
+    }
---
+++ src/oop_00000110629_RorensiaVerisca/week3/Main.kt
@@ -0,0 +1,7 @@
+package oop_00000110629_RorensiaVerisca.week3
+
+fun Main (){
+    val e = Employee("Budi")
+    println("---- Test Error ----")
+    e.salary = 5000
+}
