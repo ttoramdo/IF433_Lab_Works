95c7b1e440c57fe5207f26aa47f51bab2bc76533
ttoram <rorensiaverisca@gmail.com>
2026-02-20 11:30:40 +0700

    week03: test all encapsulation logic in main

--- src/oop_00000110629_RorensiaVerisca/week3/Main.kt
+++ src/oop_00000110629_RorensiaVerisca/week3/Main.kt
@@ -3,5 +3,11 @@ package oop_00000110629_RorensiaVerisca.week3
 fun main (){
     val e = Employee("Budi")
     println("---- Test Error ----")
-    e.salary = 5000
+
+    e.salary = -1000
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
+
+    e.increasePerformance()
+    println("Pajak yang harus dibayar: ${e.tax}")
 }
