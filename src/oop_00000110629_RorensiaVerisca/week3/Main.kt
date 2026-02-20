805672be641e745f94e32aec3b7f2ad5949385d3
ttoram <rorensiaverisca@gmail.com>
2026-02-20 21:37:54 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_00000110629_RorensiaVerisca/week3/Main.kt
+++ src/oop_00000110629_RorensiaVerisca/week3/Main.kt
@@ -10,7 +10,7 @@ fun main (){
 
     val w = Weapon("Excalibur")
 
-    println("\n---- Test Weapon ----")
+    println("\n====  Toko Senjata ====\n")
 
     w.damage = -50
     println("Damage sekarang: ${w.damage}")
@@ -19,4 +19,19 @@ fun main (){
     w.damage = 9999
     println("Damage sekarang: ${w.damage}")
     println("Tier senjata: ${w.tier}")
+
+    println("\n==== Sistem Leveling ====\n")
+
+    val p = Player("Jeonghan")
+
+    println("Username: ${p.username}")
+    println("Level awal: ${p.level}")
+
+    println("\nTambah 50 XP")
+    p.addXp(50)
+    println("Level sekarang: ${p.level}")
+
+    println("\nTambah 60 XP")
+    p.addXp(60)   // harus naik level
+    println("Level sekarang: ${p.level}")
 }
