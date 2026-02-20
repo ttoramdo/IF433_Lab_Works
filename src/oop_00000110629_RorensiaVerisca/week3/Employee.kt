5bc4526c0035526f4e1e63be6b91112ef7797cb9
ttoram <rorensiaverisca@gmail.com>
2026-02-20 11:17:24 +0700

    week03: add private property

--- src/oop_00000110629_RorensiaVerisca/week3/Employee.kt
+++ src/oop_00000110629_RorensiaVerisca/week3/Employee.kt
@@ -1,14 +1,22 @@
 package oop_00000110629_RorensiaVerisca.week3
 
-class Employee(val name: String){
+class Employee(val name: String) {
     var salary: Int = 0
-}
-
-var salary: Int = 0
-    set(value) {
-        if(value < 0){
-            println("ERROR: Gaji tidak boleh negatif! Di set ke 0.")
-        } else {
-            field = value
+        set(value) {
+            if (value < 0) {
+                println("ERROR: Gaji tidak boleh negatif! Di set ke 0.")
+            } else {
+                field = value
+            }
         }
+
+    private var performanceRating: Int = 3
+    fun increasePerformance() {
+        performanceRating++
+        println("Kinerja $name meningkat Rating: $performanceRating")
     }
+
+    fun printStatus() {
+        println("Karyawan: $name. Rating: $performanceRating")
+    }
+}
