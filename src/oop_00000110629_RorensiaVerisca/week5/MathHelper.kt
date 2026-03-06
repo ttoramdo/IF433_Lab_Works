a488babe2e0ca549919521bf892ad21e521a4aaf
ttoram <rorensiaverisca@gmail.com>
2026-03-06 11:20:37 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_00000110629_RorensiaVerisca/week5/MathHelper.kt
@@ -0,0 +1,17 @@
+package oop_00000110629_RorensiaVerisca.week5
+
+class MathHelper {
+
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+
+}
