f488465276c542254ea59325a237dccaccdddd44
ttoram <rorensiaverisca@gmail.com>
2026-03-06 11:42:54 +0700

    week05: (task 2) create abstract PaymentMethod

---
+++ src/oop_00000110629_RorensiaVerisca/week5/EWallet.kt
@@ -0,0 +1,15 @@
+package oop_00000110629_RorensiaVerisca.week5
+
+class EWallet(accName: String, var balance: Double): PaymentMethod(accName){
+    override fun processPayment(amount: Double){
+        if(balance >= amount){
+            balance -= amount
+            printlin("Pembayran berhasil menggunakan Ewallet oleh $accName")
+        } else {
+            println("Saldonya kurang kack T_________T")
+        }
+    }
+    fun topUp(amount: Double) {
+        balance += amount
+        println("$accName melakukan top up sebesar $amount")
+}
