0a87afe0b42ed32d2906c72fde4646c03c468485
ttoram <rorensiaverisca@gmail.com>
2026-03-06 10:56:18 +0700

    week05: create Dosen class inheriting Pegawai

---
+++ src/oop_00000110629_RorensiaVerisca/week5/Dosen.kt
@@ -0,0 +1,14 @@
+package oop_00000110629_RorensiaVerisca.week5
+
+class Dosen (nama: String, val nidn: String): Pegawai(nama) {
+    override fun bekerja() {
+        println("[$nama] sedang menyiapkan materi perkuliahan dan merevisi RPKPS")
+    }
+
+    fun mengajar() {
+        println("[$nama] sedang mengajar mahasiswa di kelas.")
+    }
+}
+
+
+
