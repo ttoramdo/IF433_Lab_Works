cc5dcaa5f63f3f0aa37b98300e533a41968c134f
ttoram <rorensiaverisca@gmail.com>
2026-03-13 10:50:18 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000110629_RorensiaVerisca/week6/GadgetInterfaces.kt
@@ -0,0 +1,12 @@
+package oop_00000110629_RorensiaVerisca.week6
+
+interface Camera{
+    fun turnOn(){
+        println("Lensa kamera terbuka dan sensor aktif. ")
+    }
+}
+interface Cphone{
+    fun turnOn(){
+        println("Sinyal seluler mencari jaringan. ")
+    }
+}
