44e1f3eb660a7dc80f2c7245a4766b9d8a5e6027
ttoram <rorensiaverisca@gmail.com>
2026-03-13 11:21:32 +0700

    week06: (task) create smart home interfaces

---
+++ src/oop_00000110629_RorensiaVerisca/week6/SmartInterface.kt
@@ -0,0 +1,19 @@
+package oop_00000110629_RorensiaVerisca.week6
+
+interface SmartDevice {
+    val id: String
+    val name: String
+}
+
+interface Switchable {
+    fun turnOn()
+    fun turnOff()
+}
+
+interface Recordable {
+    fun startRecord()
+
+    fun stopRecord() {
+        println("Perekaman dihentikan dan disimpan ke Cloud.")
+    }
+}
