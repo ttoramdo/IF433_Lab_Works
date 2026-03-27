5639dca8f56f372d98f1da2964b52e2a7e46bd38
ttoram <rorensiaverisca@gmail.com>
2026-03-27 11:10:44 +0700

    week07: implement copy and destructuring on data class

--- src/oop_00000110629_RorensiaVerisca/week7/Main.kt
+++ src/oop_00000110629_RorensiaVerisca/week7/Main.kt
@@ -10,14 +10,21 @@ fun main() {
     client.connect()
 
     println("\n=== TEST REGULAR CLASS ===")
-    val  reg1 = RegularUSer("Jeonghan", 30)
-    val  reg2 = RegularUSer("Jeonghan", 30)
+    val  reg1 = RegularUser("Jeonghan", 30)
+    val  reg2 = RegularUser("Jeonghan", 30)
     println(reg1)
     println("Sama? ${reg1 == reg2}")
 
     println("\n=== TEST DATA CLASS ===")
-    val  data1 = RegularUSer("Jeonghan", 30)
-    val  data2 = RegularUSer("Jeonghan", 30)
+    val  data1 = DataUser("Jeonghan", 30)
+    val  data2 = DataUser("Jeonghan", 30)
     println(data1)
     println("Sama? ${data1 == data2}")
+
+    val data3 = data1.copy(age = 31)
+    println("Hasil  Copy: $data3")
+
+    val (userName, userAge) = data1
+    println("Destructured: $userName berumur $userAge")
+
 }
--- src/oop_00000110629_RorensiaVerisca/week7/UserModels.kt
+++ src/oop_00000110629_RorensiaVerisca/week7/UserModels.kt
@@ -1,5 +1,5 @@
 package oop_00000110629_RorensiaVerisca.week7
 
-class RegularUSer(val name: String, val age : Int) {
+class RegularUser(val name: String, val age : Int) {
 }
 data class DataUser(val name: String, val age : Int){}
