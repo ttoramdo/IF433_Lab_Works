08072226677bc4a09aab45c693e7bd7793e38c0f
ttoram <rorensiaverisca@gmail.com>
2026-03-27 11:31:05 +0700

    week07: (task) create GameItem data class

--- src/oop_00000110629_RorensiaVerisca/week7/GameModels.kt
+++ src/oop_00000110629_RorensiaVerisca/week7/GameModels.kt
@@ -3,3 +3,6 @@ package oop_00000110629_RorensiaVerisca.week7
 enum class ItemRarity(val dropChance: Int) {
     COMMON(70), UNCOMMON(10), RARE(7), EPIC(3), LEGENDARY(1)
 }
+data class GameItem(
+    val name: String, val damage: Int, val rarity: ItemRarity
+)
