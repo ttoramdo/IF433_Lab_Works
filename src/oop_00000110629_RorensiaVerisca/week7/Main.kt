3ca292bee2b8c450ada03e87bf5e1a5d6538227e
ttoram <rorensiaverisca@gmail.com>
2026-03-27 11:20:07 +0700

    week07: resolve sealed class exhaustive check

--- src/oop_00000110629_RorensiaVerisca/week7/Main.kt
+++ src/oop_00000110629_RorensiaVerisca/week7/Main.kt
@@ -33,5 +33,6 @@ fun main() {
     val uiMesssage = when(response) {
         is ApiResponse.Success ->"Tampilkan: ${response.data}"
         is ApiResponse.Error ->"Munculkan Alert: ${response.message}"
+        is ApiResponse.Loading -> "Tampilkan Spinner"
     }
 }
