6f40f91ff15b4ba5a9b76b648e9916479408d5f9
ttoram <rorensiaverisca@gmail.com>
2026-03-27 10:45:40 +0700

    week07: implement data class for structural equality

--- src/oop_00000110629_RorensiaVerisca/week7/Main.kt
+++ src/oop_00000110629_RorensiaVerisca/week7/Main.kt
@@ -1,11 +1,17 @@
 package oop_00000110629_RorensiaVerisca.week7
 
 fun main() {
-       println("==== TEST SINGLETON ====")
-       println("Status: ${DatabaseManager.connectionStatus}")
-       DatabaseManager.connect()
+    println("==== TEST SINGLETON ====")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
 
-       println("\n=== TEST COMPANION OBJECT ===")
-       val client = NetworkClient.createClient()
-       client.connect()
+    println("\n=== TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createClient()
+    client.connect()
+
+    println("\n=== TEST REGULAR CLASS ===")
+    val  reg1 = RegularUSer("Jeonghan", 30)
+    val  reg2 = RegularUSer("Jeonghan", 30)
+    println(reg1)
+    println("Sama? ${reg1 == reg2}")
 }
---
+++ src/oop_00000110629_RorensiaVerisca/week7/UserModels.kt
@@ -0,0 +1,4 @@
+package oop_00000110629_RorensiaVerisca.week7
+
+class RegularUSer(val name: String, val age : Int) {
+}
