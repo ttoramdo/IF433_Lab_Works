a64d6f3d4033f516ad43f1f287bbca29321721e2
ttoram <rorensiaverisca@gmail.com>
2026-04-17 11:14:03 +0700

    week08: create mixed data collection

--- src/oop_00000110629_RorensiaVerisca/week8/UserProfile.kt
+++ src/oop_00000110629_RorensiaVerisca/week8/UserProfile.kt
@@ -1,8 +1,7 @@
 package oop_00000110629_RorensiaVerisca.week8
 
 data class UserProfile (
-    val id: String,
     val name: String,
     val email: String?,
-    val phone: String?
+    val phone: String? = null
 )
