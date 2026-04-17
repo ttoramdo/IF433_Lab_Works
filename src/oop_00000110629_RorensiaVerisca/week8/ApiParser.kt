a073b1ff1c0bac6e5ee5fa57123848a1e07722fa
ttoram <rorensiaverisca@gmail.com>
2026-04-17 22:56:31 +0700

    week08: (task) implement payment processing with !!

--- src/oop_00000110629_RorensiaVerisca/week8/ApiParser.kt
+++ src/oop_00000110629_RorensiaVerisca/week8/ApiParser.kt
@@ -18,4 +18,13 @@ class ApiParser {
             else -> null
         }
     }
+
+    fun checkout(product: Product) {
+        val id = when (product) {
+            is Electronic -> product.id
+            is Clothing -> product.id
+        }
+        val transactionId = JavaPaymentService.processPayment(id)!!
+        println("Transaction ID: $transactionId")
+    }
 }
