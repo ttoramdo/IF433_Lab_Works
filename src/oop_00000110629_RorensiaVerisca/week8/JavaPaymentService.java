6f3b9a3916899fa5cfd809953d25198894001a55
ttoram <rorensiaverisca@gmail.com>
2026-04-17 22:53:14 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000110629_RorensiaVerisca/week8/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000110629_RorensiaVerisca.week8;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) { [cite: 211]
+        return "TRX-" + productId + "-SUCCESS"; [cite: 212]
+    }
+}
