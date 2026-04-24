4a0d4fbf9007d85a3ae415f4397858ce1dbf3648
ttoram <rorensiaverisca@gmail.com>
2026-04-24 10:48:08 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000110629_RorensiaVerisca/week9/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_00000110629_RorensiaVerisca.week9
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 20, 10.0, "OPEN"),
+        TradeLog("SOLUSDT", "LONG", 5, 12.0, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 10, -2.5, "CLOSED"),
+        TradeLog("BNBUSDT", "SHORT", 15, -8.0, "OPEN")
+    )
+}
