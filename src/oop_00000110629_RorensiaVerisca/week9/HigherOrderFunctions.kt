9d97baa35b89c13331dccb7b06ecf72b1dafab3a
ttoram <rorensiaverisca@gmail.com>
2026-04-24 10:41:24 +0700

    week09: implement map HOF for data transformation

--- src/oop_00000110629_RorensiaVerisca/week9/HigherOrderFunctions.kt
+++ src/oop_00000110629_RorensiaVerisca/week9/HigherOrderFunctions.kt
@@ -5,4 +5,10 @@ fun main(){
     println("Original Data: $numbers")
     println("\n=== HOF: FILTER ===")
     val evens = numbers.filter { it % 2 == 0 }
+
+    println("\n=== HOF: MAP ===")
+    val multiplied = evens.map { it * 10 }
+    val asStrings = multiplied.map { "Rp ${it}k" }
+    println("Multiplied: $multiplied")
+    println("Formatted: $asStrings")
 }
