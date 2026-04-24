33e68ddfc803c4e569a8356bfb27ab3a4724527c
ttoram <rorensiaverisca@gmail.com>
2026-04-24 10:51:34 +0700

    week09: (task) chain sortedBy and map for worst performers

--- src/oop_00000110629_RorensiaVerisca/week9/TradingDashboard.kt
+++ src/oop_00000110629_RorensiaVerisca/week9/TradingDashboard.kt
@@ -17,4 +17,8 @@ fun main() {
     val topPerformersString = winningTrades
         .sortedByDescending { it.roe }
     .map { "WIN [${it.pair} ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)" }
+
+    val worstPerformersString = losingTrades
+        .sortedBy { it.roe }
+    .map { "LOSS [${it.pair} ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)" }
 }
