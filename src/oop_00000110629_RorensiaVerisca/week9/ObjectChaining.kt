0ad1a48dea005adc68272430f18279a3ea550742
ttoram <rorensiaverisca@gmail.com>
2026-04-24 10:44:15 +0700

    week09: create Student data class and list

---
+++ src/oop_00000110629_RorensiaVerisca/week9/ObjectChaining.kt
@@ -0,0 +1,12 @@
+package oop_00000110629_RorensiaVerisca.week9
+
+data class Student(val name: String, val gpa: Double)
+
+fun main() {
+    val students = listOf(
+    Student("Andi", 3.2),
+    Student("Budi", 3.9),
+    Student("Citra", 2.8),
+    Student("Dewi", 3.7)
+    )
+}
