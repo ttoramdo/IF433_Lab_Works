8ee316411f4362e9207c8b8a8f99f8430aad312a
ttoram <rorensiaverisca@gmail.com>
2026-05-07 12:07:11 +0700

    week10: (task) implement search logic in repository

--- src/oop_00000110629_RorensiaVerisca/week10/WalletRepository.kt
+++ src/oop_00000110629_RorensiaVerisca/week10/WalletRepository.kt
@@ -11,4 +11,22 @@ class WalletRepository<T> {
     fun getAll(): List<T> {
         return items
     }
+    class WalletRepository<T : Any> {
+
+        private val items = mutableListOf<T>()
+
+        fun add(item: T) {
+            items.add(item)
+        }
+
+        fun getAll(): List<T> {
+            return items
+        }
+
+        fun search(keyword: String): List<T> {
+            return items.filter {
+                it.toString().contains(keyword, ignoreCase = true)
+            }
+        }
+    }
 }
