16826eeed6907d135767a8907cf12b2524a772ad
ttoram <rorensiaverisca@gmail.com>
2026-05-07 12:00:40 +0700

    week10: (task) create Coin and Transaction models

---
+++ src/oop_00000110629_RorensiaVerisca/week10/WalletModels.kt
@@ -0,0 +1,11 @@
+package oop_00000110629_RorensiaVerisca.week10
+
+data class Coin(
+    val name: String,
+    val balance: Double
+)
+
+data class Transaction(
+    val id: String,
+    val amount: Double
+)
