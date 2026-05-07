1fb776cc3ae0d654fdbf7f6f94544773bc4a71fe
ttoram <rorensiaverisca@gmail.com>
2026-05-07 12:11:51 +0700

    week10: (task) populate coin repository

--- src/oop_00000110629_RorensiaVerisca/week10/CryptoDashboard.kt
+++ src/oop_00000110629_RorensiaVerisca/week10/CryptoDashboard.kt
@@ -1,7 +1,8 @@
 package oop_00000110629_RorensiaVerisca.week10
 
 fun main() {
-
     val coinRepo = WalletRepository<Coin>()
-
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 5000.0))
 }
