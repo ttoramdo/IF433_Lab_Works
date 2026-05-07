223a2954137e55b90c26cfa4eaf47c9f30b64964
ttoram <rorensiaverisca@gmail.com>
2026-05-07 11:55:09 +0700

    week10: test math operations and comparable constraints

--- src/oop_00000110629_RorensiaVerisca/week10/NumericalStorage.kt
+++ src/oop_00000110629_RorensiaVerisca/week10/NumericalStorage.kt
@@ -1,10 +1,11 @@
 package oop_00000110629_RorensiaVerisca.week10
 
-class MathBoxx<T : Number> (val value1: T, val value2: T){
-    fun sum() : Double{
+class MathBox<T: Number>(val value1: T, val value2: T){
+    fun sum(): Double{
         return value1.toDouble() + value2.toDouble()
     }
-    fun <T> getMax(a: T, b: T): T where T : Comparable<T>{
-        return if (a > b) a else b
-    }
+}
+
+fun <T> getMax(a: T, b: T): T where T: Comparable<T>{
+    return if (a > b) a else b
 }
