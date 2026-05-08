6d1f6d05f1fa82ca47e1d83c3a7518b59b2f988a
ttoram <rorensiaverisca@gmail.com>
2026-05-08 11:15:12 +0700

    week11: (task) format dashboard output using with

--- src/oop_00000110629_RorensiaVerisca/week11/SmartHomeDashboard.kt
+++ src/oop_00000110629_RorensiaVerisca/week11/SmartHomeDashboard.kt
@@ -36,4 +36,8 @@ fun main() {
     with(homeDevices) {
         println("Jumlah perangkat di dashboard: ${this.size}")
     }
+    val totalPower = homeDevices.run {
+        sumOf { it.powerLoad }
+    }
+    println("Total penggunaan daya: $totalPower Watt")
 }
