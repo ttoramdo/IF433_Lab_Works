b1c00070459e303958d58357dded5303e338e6d1
ttoram <rorensiaverisca@gmail.com>
2026-05-08 10:55:36 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000110629_RorensiaVerisca/week11/SmartHomeModels.kt
@@ -0,0 +1,5 @@
+package oop_00000110629_RorensiaVerisca.week11
+
+data class SmartDevice (var name: String, var category: String,
+                        var isOnline : Boolean = false, var powerLoad: Int = 0) {
+}
