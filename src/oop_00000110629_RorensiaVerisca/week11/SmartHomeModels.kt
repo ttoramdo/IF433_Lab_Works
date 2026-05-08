f331070d51b790ba768977af4b9e8823f626a17b
ttoram <rorensiaverisca@gmail.com>
2026-05-08 11:00:37 +0700

    week11: (task) create extension function for SmartDevice

--- src/oop_00000110629_RorensiaVerisca/week11/SmartHomeModels.kt
+++ src/oop_00000110629_RorensiaVerisca/week11/SmartHomeModels.kt
@@ -3,3 +3,8 @@ package oop_00000110629_RorensiaVerisca.week11
 data class SmartDevice (var name: String, var category: String,
                         var isOnline : Boolean = false, var powerLoad: Int = 0) {
 }
+fun SmartDevice.diagnose() : String{
+    return "[DIAGNOSTIK] $name | Kategori: $category | Status: ${
+        if (isOnline) "Online" else "Offline"
+    } | Daya: $powerLoad Watt"
+}
