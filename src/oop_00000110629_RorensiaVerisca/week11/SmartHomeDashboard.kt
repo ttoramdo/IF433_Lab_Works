b10f1f7d095425b0dfd856e601b868eba5479231
ttoram <rorensiaverisca@gmail.com>
2026-05-08 11:13:33 +0700

    week11: (task) implement device search with let

--- src/oop_00000110629_RorensiaVerisca/week11/SmartHomeDashboard.kt
+++ src/oop_00000110629_RorensiaVerisca/week11/SmartHomeDashboard.kt
@@ -23,4 +23,14 @@ fun main() {
         println("(LOG) Kamera terhubung")
         homeDevices.add(it)
     }
+    val acUnit = run {
+        SmartDevice("Daikin Inverter (Kabel 3x2.5)", "HVAC", false, 800)
+    }
+    homeDevices.add(acUnit)
+    homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
+
+    val searchResult = homeDevices.find { it.category == "Camera" }
+    searchResult?.let {
+        println(it.diagnose())
+    }
 }
