c51d3ff3cd9d5bde53e03190370773257233a0d3
ttoram <rorensiaverisca@gmail.com>
2026-05-08 10:28:03 +0700

    week11: create string extension functions

---
+++ src/oop_00000110629_RorensiaVerisca/week11/StringExtensions.kt
@@ -0,0 +1,8 @@
+package oop_00000110629_RorensiaVerisca.week11
+
+fun String.addGreeting() : String {
+    return "Hello, $this"
+}
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
