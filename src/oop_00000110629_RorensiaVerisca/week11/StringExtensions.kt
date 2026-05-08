8daad6fc21b328a96bbd0ac1e8456419ed8c96d3
ttoram <rorensiaverisca@gmail.com>
2026-05-08 10:29:09 +0700

    week11: create nullable receiver extension

--- src/oop_00000110629_RorensiaVerisca/week11/StringExtensions.kt
+++ src/oop_00000110629_RorensiaVerisca/week11/StringExtensions.kt
@@ -6,3 +6,6 @@ fun String.addGreeting() : String {
 fun String.repeatTimes(n: Int): String {
     return this.repeat(n)
 }
+fun String?.isNullOrEmptyCustom() : Boolean{
+    return this != null && this.isEmpty()
+}
