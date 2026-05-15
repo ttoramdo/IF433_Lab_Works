d00864cf1fabcae0420533236d55015817e82071
ttoram <rorensiaverisca@gmail.com>
2026-05-15 11:05:43 +0700

    week12: (task) implement food stock validation with throw

--- src/oop_00000110629_RorensiaVerisca/week12/SmartFeeder.kt
+++ src/oop_00000110629_RorensiaVerisca/week12/SmartFeeder.kt
@@ -6,6 +6,6 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
     if (isJammed) throw DispenserJamException()
     if (requestedGram > availableGram) throw FoodEmptyException(requestedGram, availableGram)
 
-    println("Kibble berhasil dikeluarkan!")
+    println("Kibble berhasil dikeluarkan!!")
     return availableGram - requestedGram
 }
