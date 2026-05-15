44fbd3cb61e4862f6aaabef8ec4343468a0bd9cd
ttoram <rorensiaverisca@gmail.com>
2026-05-15 11:03:35 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_00000110629_RorensiaVerisca/week12/SmartFeeder.kt
@@ -0,0 +1,11 @@
+package oop_00000110629_RorensiaVerisca.week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+
+    if (isJammed) throw DispenserJamException()
+    if (requestedGram > availableGram) throw FoodEmptyException(requestedGram, availableGram)
+
+    println("Kibble berhasil dikeluarkan!")
+    return availableGram - requestedGram
+}
