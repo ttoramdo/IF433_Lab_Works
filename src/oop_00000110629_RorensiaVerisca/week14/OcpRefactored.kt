455e96c8bca1e1f7804395784f53bc4205101487
ttoram <rorensiaverisca@gmail.com>
2026-05-29 10:52:38 +0700

    week14: refactor DiscountCalculator to use strategy

--- src/oop_00000110629_RorensiaVerisca/week14/OcpRefactored.kt
+++ src/oop_00000110629_RorensiaVerisca/week14/OcpRefactored.kt
@@ -1,5 +1,7 @@
 package oop_00000110629_RorensiaVerisca.week14
 
+import kotlin.apply
+
 interface DiscountStrategy {
     fun apply(price: Double): Double
 }
@@ -8,4 +10,7 @@ class StudentDiscount : DiscountStrategy {
 }
 class MemberDiscount : DiscountStrategy {
     override fun apply(price: Double) = price * 0.85
+}
+class SafeDiscountCalculator(private val strategy: DiscountStrategy){
+    fun calculate(price: Double) = strategy.apply(price)
 }
